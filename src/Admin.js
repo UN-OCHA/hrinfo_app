c5461cbab7ecb67df1652b5063a1f409e7a383d7
Guillaume Viguier <guillaume@viguierjust.com>
2018-06-05 17:41:39 +0200

    HRINFO-804 - reactstrap usage

--- src/Admin.js
+++ src/Admin.js
@@ -8,7 +8,6 @@ class Admin extends React.Component {
           <p>What would you like to do ?</p>
           <ul>
             <li><a href="/documents/new">Add a document</a></li>
-            <li><a href="/infographics/new">Add an infographic</a></li>
           </ul>
         </div>
       );
