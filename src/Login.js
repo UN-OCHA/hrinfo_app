783344b2253e53a69b91a32a00a8ff6833692b90
Barbara <barbara.reliefapps@gmail.com>
2018-07-09 14:23:08 +0200

    cleaned imports

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
 import HRInfoAPI from './HRInfoAPI';
 import HidAPI from './HidAPI';
 
