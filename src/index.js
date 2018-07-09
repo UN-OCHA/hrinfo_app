909cee6aceab6fd8e5fde9eb1d40fd21c4bd5c9b
Barbara <barbara.reliefapps@gmail.com>
2018-07-09 13:00:38 +0200

    defined material theme

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
-import { CookiesProvider } from 'react-cookie';
+import {BrowserRouter as Router} from "react-router-dom";
+import {CookiesProvider} from 'react-cookie';
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './unocha-icons.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: blue,
+		secondary: red,
+
+	}
+});
+
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
-  <Router>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </Router>,
-  document.getElementById("root")
-);
+	<MuiThemeProvider theme={theme}>
+		<Router>
+			<CookiesProvider>
+				<App/>
+			</CookiesProvider>
+		</Router>
+	</MuiThemeProvider>,
+	document.getElementById("root"));
 registerServiceWorker();
